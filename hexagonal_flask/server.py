885de7272d553cba2a50dfca274b827ba5f62f3f
Douglas Silva <dogusky.silva@outlook.com>
2022-03-20 00:07:08 -0300

    refactor: change structure to startup application

--- hexagonal_flask/server.py
+++ hexagonal_flask/server.py
@@ -1,11 +1,24 @@
+import os
 from flask import Flask
-import hexagonal_flask.controllers.leagues as controllers_leagues
 from flask import jsonify
+import hexagonal_flask.controllers.leagues as controllers_leagues
 
-app = Flask(__name__, instance_relative_config=True)
-# app.config.from_object('config')
-app.config.from_pyfile('config.py')
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY='douglas')
 
-@app.route("/")
-def home():
-    return jsonify(controllers_leagues.english_leagues())
+    if test_config:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    @app.route('/')
+    def hello():
+        return jsonify(controllers_leagues.english_leagues())
+
+    return app
