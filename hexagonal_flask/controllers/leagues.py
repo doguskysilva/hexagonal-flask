f06fe7189d49bc10123058e4bb4a45b65f547943
Douglas Silva <dogusky.silva@outlook.com>
2022-03-20 00:29:36 -0300

    feat: add filter to controller

--- hexagonal_flask/controllers/leagues.py
+++ hexagonal_flask/controllers/leagues.py
@@ -1,5 +1,8 @@
-from hexagonal_flask.diplomat import http_out 
+from hexagonal_flask.diplomat import http_out
+import hexagonal_flask.logic.league as logic_league
 
 def english_leagues():
-    english_leagues = http_out.football_league("uk")
+    english_leagues = logic_league.filter_english_leagues(
+        http_out.football_league("UK")
+    )
     return english_leagues
