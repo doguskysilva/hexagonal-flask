492d65f7809635f2a9d6dec34b5705245df944f8
Douglas Silva <dogusky.silva@outlook.com>
2022-03-20 00:30:03 -0300

    feat: implement upper to contry im filter

--- hexagonal_flask/logic/league.py
+++ hexagonal_flask/logic/league.py
@@ -1,7 +1,7 @@
 def filter_english_leagues(leagues: list):
     return list(
         filter(
-            lambda league: league['country'] == "uk",
+            lambda league: league['country'].upper() == "UK",
             leagues
         )
     )
